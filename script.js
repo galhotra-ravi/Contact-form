9bb4cea39ac4f352730d1f59f699db12c81b436c
Ravi Galhotra <140328099+galhotra-ravi@users.noreply.github.com>
2024-04-19 10:05:52 +0530

    added loader

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-var form = document.querySelector('.container form');
-  form.addEventListener("submit", e => {
-    e.preventDefault();
-    fetch(form.action, {
-        method : "POST",
-        body: new FormData(document.querySelector(".container form")),
-    }).then(
-        response => response.json()
-    ).then((html) => {
-      // you can put any JS code here
-      alert('Form Submitted Succesfully!')
-    });
+let form = document.querySelector('.container form');
+let loader = document.querySelector(".loader");
+let button = document.querySelector("button");
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+
+  loader.classList.remove('hidden');
+
+  fetch(form.action, {
+    method: "POST",
+    body: new FormData(document.querySelector(".container form")),
+  }).then(
+    response => response.json()
+  ).then((html) => {
+
+    loader.classList.add('hidden');
+    button.innerText = "Sent ✅"
+    
+    setInterval(() => {
+      form.reset(); 
+      button.innerText = "Submit"
+    }, 2000);
+    // alert('Form Submitted Succesfully!')
   });
+});
